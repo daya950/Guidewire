4f316427c844750fe0fcf69c71b5d2b4cc2c93d7
daya950@gmail.com <infogain123>
2016-11-08 13:14:00 +0530

    4

--- Abc/src/com/infogain/fnol/integration/policy/GwMain.java
+++ Abc/src/com/infogain/fnol/integration/policy/GwMain.java
@@ -21,6 +21,7 @@ import com.guidewire.pc.ws.gw.webservice.pc.pc800.ccintegration.ccpolicysearchin
 /**
  * Servlet implementation class GwMain
  */
+@WebServlet(name = "main",urlPatterns = {"/"})
 public class GwMain extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
