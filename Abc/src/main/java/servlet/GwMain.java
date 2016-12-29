6304b1a7738a6c99ca61798c111f77103c87b21e
daya950 <daya950@gmail.com>
2016-12-29 14:13:50 +0530

    Update GwMain.java

--- Abc/src/main/java/servlet/GwMain.java
+++ Abc/src/main/java/servlet/GwMain.java
@@ -220,10 +220,10 @@ public class GwMain extends HttpServlet {
 				fnoljsondto.setVehicleInfoDTO(vehDTO);
 				fnoljsondto.setLossDetailDTO(lossDetailDto);
 
-				//ClaimDTO objClaimDTO = ws.save(fnoljsondto);
-				//json.put("claimNo", objClaimDTO.getClaimNumber());
-				//out.print(json);
-				out.print(ws.save(fnoljsondto));
+				ClaimDTO objClaimDTO = ws.save(fnoljsondto);
+				json.put("claimNo", objClaimDTO.getClaimNumber());
+				out.print(json);
+				//out.print(ws.save(fnoljsondto));
 			} catch(Exception ex){
 				out.print(ex.getMessage());                   
 			}
